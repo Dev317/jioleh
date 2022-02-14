de08cc4e375456083df7a4748f624dcfba61d5a4
Jenpoer <jeistory1@gmail.com>
2022-02-14 15:58:20 +0800

    Added vendor sign up and login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import VendorScanner from "./components/VendorScanner";
+import VendorLogin from "./components/VendorLogin";
+import VendorSignUp from "./components/VendorSignUp";
 import Login from "./components/Login";
 import Home from "./container/Home";
 
@@ -11,15 +13,18 @@ const App = () => {
     const User =
       localStorage.getItem("user") !== "undefined"
         ? JSON.parse(localStorage.getItem("user"))
+        : localStorage.getItem("vendor") !== "undefined"
+        ? JSON.parse(localStorage.getItem("vendor"))
         : localStorage.clear();
-
     if (!User) navigate("/login");
   }, []);
 
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="vendor-scanner" element={<VendorScanner />} />
+      <Route path="/vendor-scanner" element={<VendorScanner />} />
+      <Route path="/vendor-login" element={<VendorLogin />} />
+      <Route path="/vendor-signup" element={<VendorSignUp />} />
       <Route path="/*" element={<Home />} />
     </Routes>
   );
--- frontend/src/components/VendorScanner.jsx
+++ frontend/src/components/VendorScanner.jsx
@@ -30,33 +30,44 @@ const Scanner = (props) => {
   };
 
   return (
-    <div className="flex h-full items-center flex-col">
+    <div className="flex items-center flex-col">
       <div id="reader" width="100%" />
-      <button
-        type="button"
-        onClick={() => handleClickAdvanced()}
-        className="bg-red-500 text-white font-bold p-2 rounded-full w-28 outline-none"
-      >
-        Scan
-      </button>
-      <button onClick={() => props.stop()}>Stop</button>
+      <div className="pt-3 flex flex-col">
+        <button
+          type="button"
+          onClick={() => handleClickAdvanced()}
+          className="bg-red-500 text-white font-bold p-4 rounded-full w-28 outline-none"
+        >
+          Scan
+        </button>
+        <button
+          type="button"
+          onClick={() => props.stop()}
+          className="bg-primary text-black font-bold p-4 rounded-full w-28 outline-1"
+        >
+          Stop
+        </button>
+      </div>
     </div>
   );
 };
 
 export default function VendorScanner() {
-  const [errorMessage, setErrorMessage] = useState({
+  const [alertMessage, setAlertMessage] = useState({
     message: "",
+    error: false,
     show: false,
   });
 
   useEffect(() => {
-    if (errorMessage.show) {
+    if (alertMessage.show) {
       new Promise((r) => {
-        setTimeout(r, 15000);
-      }).then(() => setErrorMessage({ message: "", show: false }));
+        setTimeout(r, 6000);
+      }).then(() =>
+        setAlertMessage({ message: "", error: false, show: false })
+      );
     }
-  }, [errorMessage.show]);
+  }, [alertMessage.show]);
 
   const handleStop = () => {
     try {
@@ -84,7 +95,8 @@ export default function VendorScanner() {
         const poster = url.searchParams.get("poster");
         const query =
           '*[_type == "qr_scanned" && viewer == $viewer && vendor == $vendor]';
-        const params = { viewer: viewer, vendor: SAMPLE_VENDOR };
+        const vendor = JSON.parse(localStorage.getItem("vendor"));
+        const params = { viewer: viewer, vendor: vendor._id };
 
         client.fetch(query, params).then((qrScanned) => {
           if (qrScanned.length > 0) {
@@ -94,7 +106,7 @@ export default function VendorScanner() {
               _type: "qr_scanned",
               viewer: viewer,
               poster: poster,
-              vendor: SAMPLE_VENDOR,
+              vendor: vendor._id,
             };
 
             client
@@ -103,10 +115,10 @@ export default function VendorScanner() {
                 client
                   .patch(viewer)
                   .setIfMissing({ visitedPlacesId: [] })
-                  .append("visitedPlacesId", [SAMPLE_VENDOR])
+                  .append("visitedPlacesId", [vendor._id])
                   .commit()
                   .then((res) => {
-                    resolve(JSON.stringify(res));
+                    resolve("Successfully redeemed!");
                   })
                   .catch((err) => {
                     reject(err);
@@ -124,22 +136,70 @@ export default function VendorScanner() {
   function handleOnResult(res) {
     handleScanQR(res)
       .then((r) => {
-        setErrorMessage({ message: r, show: true });
+        setAlertMessage({ message: r, error: false, show: true });
       })
       .catch((err) => {
-        setErrorMessage({ message: err.message, show: true });
+        setAlertMessage({ message: err.message, error: true, show: true });
       });
   }
 
   return (
-    <div className="relative pb-2 h-full justify-center items-center">
-      <Scanner
-        onResult={(res) => {
-          handleOnResult(res);
-        }}
-        stop={handleStop}
-      />
-      {errorMessage.show ? <p>{errorMessage.message}</p> : <div></div>}
+    <div className="relative pb-5 h-screen flex flex-col justify-center items-center">
+      <div class="p-5 max-w-sm rounded overflow-hidden shadow-lg">
+        <Scanner
+          onResult={(res) => {
+            handleOnResult(res);
+          }}
+          stop={handleStop}
+        />
+      </div>
+      {alertMessage.show ? (
+        alertMessage.error ? (
+          <div
+            className="flex mt-3 p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800"
+            role="alert"
+          >
+            <svg
+              className="inline flex-shrink-0 mr-3 w-5 h-5"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
+            <span className="font-medium">{alertMessage.message}</span>
+          </div>
+        ) : (
+          <div
+            className="flex mt-3 p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800"
+            role="alert"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span className="font-medium">{alertMessage.message}</span>
+          </div>
+        )
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
