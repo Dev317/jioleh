4e9d3869735b1d2ece33459e079ed4e0cec8d430
Minh Vu <minhvu@Minhs-MacBook-Pro.local>
2022-02-27 21:36:52 +0800

    update before merging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+    const User = fetchUser();
     if (!User) navigate('/login');
   }, []);
 
