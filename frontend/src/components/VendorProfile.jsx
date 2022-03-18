03aea83e21cff8f5306376c00b66039dd94d91fa
shelle-mim <michelleleong8@gmail.com>
2022-03-19 03:52:12 +0800

    UI adjustments

--- frontend/src/components/VendorProfile.jsx
+++ frontend/src/components/VendorProfile.jsx
@@ -11,14 +11,109 @@ import QRModal from './QRModal';
 import { vendorQuery } from '../utils/data';
 
 const randomImg = 'https://cdn.wallpapersafari.com/29/50/7acBKo.jpg';
-const activeBtnStyles = 'bg-red-500 text-white font-bold p-2 rounded-full w-20 outline-none';
-const notActiveBtnStyles = 'bg-primary mr-4 text-black font-bold p-2 rounded-full w-20 outline-none';
+const activeBtnStyles = "p-2 font-bold border-b-2 w-20 outline-solid border-red-500 text-red-500";
+const notActiveBtnStyles = "bg-primary text-black w-20 font-bold p-2 outline-none";
+const EditBtnStyle = 'bg-red-500 text-white font-bold p-2 rounded-full w-20 outline-none';
+
+const About = ({vendor, editingMode, setEditingMode}) => {
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+  const [location, setLocation] = useState("");
+  const [description, setDescription] = useState("");
+
+  useEffect(() => { setName(vendor.name) }, [])
+  
+  return (
+    <div className='text-center mb-7'>
+        <button
+            type="button"
+            onClick={(e) => {
+              setEditingMode(!editingMode);
+            }}
+            className={EditBtnStyle}
+          >
+            {editingMode ? "Save" : "Edit Profile"}
+        </button>
+
+        {editingMode ? 
+          <form class="w-full p-6">
+            <div class="mb-6">
+              <label class="text-left block text-gray-700 text-sm font-bold mb-2" for="name">
+                Company Name
+              </label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Company Name" value={name} onChange={e => setName(e.target.value)}/>
+            </div>
+            <div class="mb-6">
+              <label class="text-left block text-gray-700 text-sm font-bold mb-2" for="category">
+                Category
+              </label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="category" type="text" placeholder="Category" />
+            </div>
+            <div class="mb-6">
+              <label class="text-left block text-gray-700 text-sm font-bold mb-2" for="Location">
+                Location
+              </label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="location" placeholder="Location" />
+            </div>
+            <div class="mb-6">
+              <label class="text-left block text-gray-700 text-sm font-bold mb-2" for="Description">
+                Description
+              </label>
+              <textarea
+                class="
+                  form-control
+                  block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+                "
+                id="description"
+                rows="4"
+                placeholder="Description"
+              ></textarea>
+            </div>
+          </form> 
+        :
+          <div class="grid grid-cols-3 gap-2" >
+            <div class="...">
+              <p className="text-xl font-bold break-words mt-3">Category:</p>
+            </div>
+            <div class="col-span-2 ...">
+              <p className="text-xl break-words mt-3">{vendor.category == null ? "no text" : vendor.category}</p>
+            </div>
+            <div class="...">
+              <p className="text-xl font-bold break-words mt-3">Location:</p>
+            </div>
+            <div class="col-span-2 ...">
+              <p className="text-xl break-words mt-3">{vendor.location == null ? "no text" : vendor.location}</p>
+            </div><div class="...">
+              <p className="text-xl font-bold break-words mt-3">About:</p>
+            </div>
+            <div class="col-span-3 ...">
+              <p className="text-xl break-words mt-3">{vendor.description == null ? "no text" : vendor.description}</p>
+            </div>
+          </div>
+        }
+
+    </div>
+  )
+}
 
 const VendorProfile = () => {
   const [vendor, setVendor] = useState(null);
   const [pins, setPins] = useState(null);
-  const [text, setText ] = useState('Created');
-  const [activeBtn, setActiveBtn] = useState('created');
+  const [text, setText ] = useState('about');
+  const [activeBtn, setActiveBtn] = useState('about');
   const [showQRModal, setShowQRModal] = useState(false);
   const [editingMode, setEditingMode] = useState(false);
   const navigate = useNavigate();
@@ -30,20 +125,6 @@ const VendorProfile = () => {
       setVendor(data[0]);
     });
   }, [vendorId]);
-
-  // useEffect(() => {
-  //   if (text === 'Created') {
-  //     const createdPinsQuery = CreatedPinsQuery(userId);
-  //     client.fetch(createdPinsQuery).then((data) => {
-  //       setPins(data);
-  //     })
-  //   } else {
-  //     const savedPinsQuery = userSavedPinsQuery(userId);
-  //     client.fetch(savedPinsQuery).then((data) => {
-  //       setPins(data);
-  //     });
-  //   }
-  // }, [text, userId]);
 
   const logout = () => {
     localStorage.clear();
@@ -77,109 +158,52 @@ const VendorProfile = () => {
           </h1>
 
           <div className='absolute top-0 z-1 right-0 p-2'>logout
-          {/* {userId === user._id && (
-              <GoogleLogout
-                clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
-                render={(renderProps) => (
-                  <button
-                    type="button"
-                    className=" bg-white p-2 rounded-full cursor-pointer outline-none shadow-md"
-                    onClick={renderProps.onClick}
-                    disabled={renderProps.disabled}
-                  >
-                    <AiOutlineLogout color="red" fontSize={21} />
-                  </button>
-                )}
-                onLogoutSuccess={logout}
-                cookiePolicy="single_host_origin"
-              />
-            )} */}
-
+          {vendorId === vendor._id && (
             <button
               type="button"
               className=" bg-white p-2 rounded-full cursor-pointer outline-none shadow-md"
+              onClick={logout}
+              // disabled={renderProps.disabled}
             >
               <AiOutlineLogout color="red" fontSize={21} />
             </button>
+            )}
           </div>
         </div>
 
-        <div className='text-center mb-7'>
+        <div className="text-center mb-7">
           <button
-              type="button"
-              onClick={(e) => {
-                setEditingMode(!editingMode);
-              }}
-              className={`${activeBtn === 'created' ? activeBtnStyles : notActiveBtnStyles}`}
-            >
-              {editingMode ? "Save" : "Edit Profile"}
+            type="button"
+            onClick={(e) => {
+              setText(e.target.textContent);
+              setActiveBtn("about");
+            }}
+            className={`${
+              activeBtn === "about" ? activeBtnStyles : notActiveBtnStyles
+            }`}
+          >
+            About
           </button>
-
-          {editingMode ? 
-          <form class="w-full max-w-sm">
-          <div class="mb-6">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="category">
-              Category
-            </label>
-            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="category" type="text" placeholder="Category" />
-          </div>
-          <div class="mb-6">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="Location">
-              Location
-            </label>
-            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="location" placeholder="Location" />
-          </div>
-          <div class="mb-6">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="Location">
-              Description
-            </label>
-            <textarea
-              class="
-                form-control
-                block
-                w-full
-                px-3
-                py-1.5
-                text-base
-                font-normal
-                text-gray-700
-                bg-white bg-clip-padding
-                border border-solid border-gray-300
-                rounded
-                transition
-                ease-in-out
-                m-0
-                focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-              "
-              id="description"
-              rows="4"
-              placeholder="Description"
-            ></textarea>
-          </div>
-          </form> 
-          :
-          <div class="grid grid-cols-3 gap-2" >
-            <div class="...">
-              <p className="text-xl font-bold break-words mt-3">Category:</p>
-            </div>
-            <div class="col-span-2 ...">
-              <p className="text-xl break-words mt-3">{vendor.category == null ? "no text" : vendor.category}</p>
-            </div>
-            <div class="...">
-              <p className="text-xl font-bold break-words mt-3">Location:</p>
-            </div>
-            <div class="col-span-2 ...">
-              <p className="text-xl break-words mt-3">{vendor.location == null ? "no text" : vendor.location}</p>
-            </div><div class="...">
-              <p className="text-xl font-bold break-words mt-3">About:</p>
-            </div>
-            <div class="col-span-3 ...">
-              <p className="text-xl break-words mt-3">{vendor.description == null ? "no text" : vendor.description}</p>
-            </div>
-          </div>
-          }
-          
+          <button
+            type="button"
+            onClick={(e) => {
+              setText(e.target.textContent);
+              setActiveBtn("campaigns");
+            }}
+            className={`${
+              activeBtn === "campaigns" ? activeBtnStyles : notActiveBtnStyles
+            }`}
+          >
+            Campaigns
+          </button>
         </div>
+
+        {activeBtn == "about" ? 
+          <About vendor={vendor} editingMode={editingMode} setEditingMode={setEditingMode}/>
+          :
+          <div class='text-center'>Campaign Information</div>
+        }
+        
 
       </div>
     </div>
